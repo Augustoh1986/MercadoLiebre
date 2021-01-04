73a924b038bca73527be852dbae4dcee9743630e
Augusto Bidart <abidart@ml.com>
2021-01-04 17:15:30 -0300

    subir

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+
+app.listen(8080, () => { 
+    console.log('-----------------------------------------------')
+    console.log('Todo salio OK! Ya podés ir a http://localhost:8080');
+    console.log('-----------------------------------------------')
+});
